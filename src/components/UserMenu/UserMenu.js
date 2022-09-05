75408d422d4c5e3074007676ab0423e0c9b6e26f
Olyazarichna <olya.zarichna@gmail.com>
2022-09-05 09:19:20 +0300

    usermenu

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -2,12 +2,18 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from 'redux/authSlice';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
+import { contactsApi } from 'services/contactsApi';
 export const UserMenu = () => {
   const { email } = useSelector(state => state.auth);
   const dispatch = useDispatch();
-
+ 
+console.log(contactsApi);
   const handleClick = () => {
+    
+   
     dispatch(logout());
+   dispatch(contactsApi.util.resetApiState()) ;
   };
   return (
     <>
